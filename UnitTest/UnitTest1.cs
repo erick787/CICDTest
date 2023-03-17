77aa166733ada9cd2afc06aa4ae1e802ee41986a
Erick Gomez <egomez@kisp.com>
2023-03-17 14:07:36 -0700

    Added unit test

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -5,9 +5,12 @@ namespace UnitTest
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        [TestMethod, TestCategory("This test checks whether a number is even or not")]
+        public void IsAnEvenNumber()
         {
+            int number = 2;
+            number += 5;
+            Assert.IsTrue(number % 2 == 0);
         }
     }
 }
